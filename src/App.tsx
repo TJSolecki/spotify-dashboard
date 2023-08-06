b717db653cf1d50136a55a67bc18475416d78279
Thomas Solecki <thomasjsolecki@gmail.com>
2023-08-06 10:40:45 -0700

    [frontend] Add conditional rendering for views in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
+import { useState, useEffect } from "react";
+import Login from "./views/Login.tsx";
+import Dashboard from "./views/Dashboard.tsx";
+import axios from "axios";
 function App() {
+  const [auth, setAuth] = useState<boolean>(false);
+  useEffect(() => {
+    axios.get('/check-auth')
+      .then(({data}) => {
+        if (data?.isAuth) {
+          return setAuth(true);
+        }
+        setAuth(false);
+      })
+  }, [])
 
   return (
-    <h1>Hello World</h1>   
+    <div className="App">
+      {!auth ? <Login/> : <Dashboard/>}
+    </div>
   );
 }
 
 export default App
+
---
+++ src/views/Dashboard.tsx
@@ -0,0 +1,9 @@
+import TopArtistTable from "../componets/TopArtistTable.tsx"
+
+function Dashboard() {
+  return (
+    <TopArtistTable/>
+  );
+}
+
+export default Dashboard;
