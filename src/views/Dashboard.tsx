d2cc464e95f8502dca72fc7149c9a9ad98786d7d
Thomas Solecki <thomasjsolecki@gmail.com>
2023-08-12 18:25:15 -0700

    [frontend] Integrate TopTracksTable into Dashboard view

--- src/views/Dashboard.tsx
+++ src/views/Dashboard.tsx
@@ -1,8 +1,36 @@
-import TopArtistTable from "../componets/TopArtistTable.tsx"
+import { useState } from "react";
+import TopArtistsTable from "../componets/TopArtistTable.tsx";
+import TopTracksTable from "../componets/TopTracksTable.tsx";
+
+type TabType = "artists" | "tracks"
 
 function Dashboard() {
+  const [ artistsOrTracks, setArtistsOrTracks ] = useState<TabType>("artists");
   return (
-    <TopArtistTable/>
+    <>
+      <div className="tabs tabs-boxed">
+        <a
+          onClick={() => setArtistsOrTracks("artists")}
+          className={`tab ${artistsOrTracks === "artists" ? "tab-active" : ""}`}
+        >
+          Top Artists
+        </a>
+        <a
+          onClick={() => setArtistsOrTracks("tracks")}
+          className={`tab ${artistsOrTracks === "tracks" ? "tab-active" : ""}`}
+        >
+          Top Tracks
+        </a>
+      </div>
+      <main>
+        {artistsOrTracks === "artists"
+          ?
+            <TopArtistsTable/>
+          :
+            <TopTracksTable/>
+        }
+      </main>
+    </>
   );
 }
 
