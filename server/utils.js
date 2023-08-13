b06a868ca790aaf4631a59f394c0e6ec2d6f02b6
Thomas Solecki <thomasjsolecki@gmail.com>
2023-08-12 18:22:33 -0700

    [backend] Add functionality to refresh spotify access token

--- server/utils.js
+++
@@ -1,9 +0,0 @@
-export function generateRandomString(length) {
-  let text = '';
-  let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-
-  for (let i = 0; i < length; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-  return text;
-}
